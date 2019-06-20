0ead07d92c0d52bb180fc1ee6e6bbaec4f23ba1f
GasparErrobidart <podmaster@hotmail.com>
2019-06-20 12:03:56 -0300

    add scene remove

--- demo/platformer/Doughnut.java
+++ demo/platformer/Doughnut.java
@@ -10,10 +10,11 @@ public class Doughnut extends Component {
   private String coinSoundPath = System.getProperty("user.dir")+"/demo/platformer/sounds/coin.wav";
   private File coinSound = new File(coinSoundPath);
   private Clip loadedCoinSound;
+  private Scene scene;
 
 
   public Doughnut(){
-    Scene scene = Scene.getInstance();
+    this.scene = Scene.getInstance();
     // LOAD SPRITE SHEET
     loadBitmap(System.getProperty("user.dir")+"/demo/platformer/images/doughnut.png");
     // SET COMPONENT FRAME SIZE
@@ -27,8 +28,8 @@ public class Doughnut extends Component {
   public void onCollisionStart(Collision collision){
     Component component = collision.getOther(getId());
     if(component.getTag() == "player"){
-      this.playSound(loadedCoinSound); 
-      this.move(-9999,-9999);
+      this.playSound(loadedCoinSound);
+      scene.remove(this);
     }
   }
 
@@ -36,7 +37,7 @@ public class Doughnut extends Component {
   public void playSound(Clip clip) {
     if(clip != null){
       clip.setMicrosecondPosition(350000);
-      clip.start();      
+      clip.start();
     }
   }
 
